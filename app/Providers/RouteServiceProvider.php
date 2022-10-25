1ffc4d01a5ff496da3f8080e57367ccc9d6ae585
Dulon18 <dulon587@gmail.com>
2022-10-25 22:29:54 +0600

    authentication

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
-
+    
+    public const HOME = '/admin/dashboard';
+    public const USER = '/user/dashboard';
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/admin/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/user/dashboard', function () {
+    return view('home');
+})->middleware(['auth'])->name('home');
 
 require __DIR__.'/auth.php';
